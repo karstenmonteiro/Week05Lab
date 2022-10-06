d4437ff0b8073d15040cb57f54e49138080591c4
karstenmonteiro <karstenmonteiro@kmLaptop>
2022-10-06 00:39:59 -0600

    Week 5 Lab: MyLogin (initial commit) The initial commit for the week 5 lab, MyLogin.

---
+++ src/java/servlets/LoginServlet.java
@@ -0,0 +1,40 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Karsten Monteiro
+ */
+public class LoginServlet extends HttpServlet {
+
+    /**
+     * 
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException 
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+
+    /**
+     * 
+     * @param request
+     * @param response
+     * @throws ServletException
+     * @throws IOException 
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    }
+}
